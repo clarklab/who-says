cefa689ea7c7c3fac63f196e1335183405bdf7fc
Clark Wimberly <clark@clarklab.net>
2015-07-17 18:25:03 -0500

    init

---
+++ index.js
@@ -0,0 +1,34 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/middle', function(req, res){
+  res.sendFile(__dirname + '/index-middle.html');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+
+io.on('connection', function(socket){
+  socket.on('clear', function(){
+    io.emit('clear');
+  });
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
